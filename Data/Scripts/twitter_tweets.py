1da3e0f26e049527c96fe71c2fccbc82e70e6eee
Malon van Essen <malon242@hotmail.com>
2020-06-01 22:25:35 +0200

    Scraped tweets for all genders

--- Data/Scripts/twitter_tweets.py
+++ Data/Scripts/twitter_tweets.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 
 import csv
+import time
+import tweepy
 import pandas as pd
+
+def credentials():
+	"""Reads consumer keys and access tokens from twitter_keys.txt file"""
+	with open('twitter_keys.txt', 'r') as f:
+		lines = f.readlines()
+
+	auth = tweepy.OAuthHandler(lines[0].split('=')[1].strip(), 
+		lines[1].split('=')[1].strip())
+	auth.set_access_token(lines[2].split('=')[1].strip(), 
+		lines[3].split('=')[1].strip())
+
+	api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+	return api
+
 
 def read_data():
 	"""Read csv files containing usernames and status counts"""
@@ -21,35 +38,46 @@ def filter_users(lst):
 	for frame in lst:
 		frame = frame.drop_duplicates()
 		frame = frame[frame['status_count'] > 1000]
-		frame = frame.drop(frame.index[299:-1])
+		frame = frame.drop(frame.index[199:-1]).reset_index(drop=True)
 		new_lst.append(frame)
 
 	return new_lst
 
 
-def get_tweets(df, file):
+def tweets(csv_writer, row, api):
+	print(row.name, row['username'])
+
+	try:
+		for tweet in tweepy.Cursor(api.user_timeline, screen_name=row['username'], 
+			exclude_replies=True, include_rts=False).items(200):
+			csv_writer.writerow([row.name, row['username'], tweet.text])
+	except tweepy.TweepError as e:
+		if e == "[{u'message': u'Rate limit exceeded', u'code': 88}]":
+			time.sleep(60*5) #Sleep for 5 minutes
+		else:
+			print(e)
+
+
+def get_tweets(df, file, api):
 	"""Get 250 per user, except retweets and replies, and write to file"""
 	filename = '../Twitter Data/{}'.format(file)
 	header = ['id', 'username', 'tweet']
 	with open(filename, 'a+') as f:
 		csv_writer = csv.writer(f)
 		csv_writer.writerow(header)
-		print("OK")
-#		for user in tweepy.Cursor(api.search_users, "they/them").items(1000):
-#			nb.write('{}, {}\n'.format(user.screen_name, user.statuses_count))
-# id, screen_name, tweet.text
-
+		df.apply(lambda row: tweets(csv_writer, row, api), axis=1)
 
 
 def main():
+	api = credentials()
 	users_nb, users_m, users_f = read_data()
 
 	raw_frames = [users_nb, users_m, users_f]
 	filtered_frames = filter_users(raw_frames)
 
-	get_tweets(filtered_frames[0], 'tw_tweets_nb.csv') # NON-BINARY
-
-
+	get_tweets(filtered_frames[0], 'tw_tweets_nb.csv', api) # NON-BINARY
+	get_tweets(filtered_frames[1], 'tw_tweets_m.csv', api) # MALE
+	get_tweets(filtered_frames[2], 'tw_tweets_f.csv', api) # FEMALE
 
 
 if __name__ == '__main__':
